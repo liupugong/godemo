71b18777114557d5fa569f50d2a9c652d3eaf32d
liupugong <pugong.liu@gmail.com>
2015-11-26 08:16:10 +0800

    fix some typo

--- hello/hello.go
+++ hello/hello.go
@@ -6,6 +6,10 @@ import (
     "github.com/liupugong/godemo/stringutil"
 )
 
+func demo() {
+	fmt.Printf("hello, pretty!")
+}
+
 func main() {
     fmt.Printf(stringutil.Reverse("!oG ,olleH"))
     fmt.Printf("\n")
